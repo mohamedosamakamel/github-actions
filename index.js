775745c3991ba0b9e01033c941a08c304de096c6
mohamed osama <Mohamed.Osama@qara.net>
2023-07-17 11:50:10 +0200

    GitHub  Actions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const moment = require("moment/moment");
 
 console.log("HELLO");
-console.log("Moment now :", moment(moment.now()).format())
+console.log("Moment now :", moment(moment.now()).format())  
